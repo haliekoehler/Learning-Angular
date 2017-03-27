9c352a15046c10fd9c916b9343e44be2fe18e08f
haliekoehler <koehlerdoom@gmail.com>
2017-03-27 15:28:07 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Learning Angular
+
+## This project is : *In Progress*
+
+**Project direction taken from Codeup's currirculum (Appendix A)** 
---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by HKoehler on 3/27/17.
+ */
+
+'use strict';
+
+(function (){
+
+    var app = angular.module("index", []);
+
+
+})();
